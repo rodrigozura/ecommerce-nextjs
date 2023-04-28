d4146fe69d0cb4aa1328d811ba624927f0f6e081
rodrigozura <rodrigozura@gmail.com>
2023-04-28 18:32:18 -0300

    admin authentication

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
-  return (
-    <div className={'bg-blue-900 w-screen h-screen flex items-center'}>
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
+            Login with Google
+          </button>
+        </div>
       </div>
+    );
+  }
+
+  return (
+    <div>
+      logged in {session.user.email}
+      <button
+        onClick={() => signOut()}
+        className="bg-white p-2 px-4 rounded-lg"
+      >
+        Log Out
+      </button>
     </div>
-  )
+  );
 }
