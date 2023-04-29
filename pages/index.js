97eb20f2e16d62cb827904d3a16d0e3ac0cba0fb
rodrigozura <rodrigozura@gmail.com>
2023-04-29 14:30:05 -0300

    admin panel basic design

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,20 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 px-4 rounded-lg"
-          >
-            Login with Google
-          </button>
+  if (!session) return;
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 text-black rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt="" className="w-6 h-6" />
+          <span className="px-2">{session?.user?.name}</span>
         </div>
       </div>
-    );
-  }
-
-  return (
-    <div>
-      logged in {session.user.email}
-      <button
-        onClick={() => signOut()}
-        className="bg-white p-2 px-4 rounded-lg"
-      >
-        Log Out
-      </button>
-    </div>
+    </Layout>
   );
 }
---
+++ pages/products.js
@@ -0,0 +1,5 @@
+import Layout from "@/components/Layout";
+
+export default function Product() {
+  return <Layout>Products</Layout>;
+}
