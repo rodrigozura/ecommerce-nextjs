9df6eacab90dd8bf6027c341ab348d8cd7311b47
rodrigozura <rodrigozura@gmail.com>
2023-04-29 19:34:30 -0300

    Product model fixed and redirect to product after create a new product

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,17 +1,23 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function NewProduct() {
+  const router = useRouter();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
+  const [goToProduct, setGoToProduct] = useState(false);
 
   async function createProduct(evt) {
     evt.preventDefault();
     const data = { title, description, price };
     await axios.post("/api/products", data);
+    setGoToProduct(true);
   }
+
+  if (goToProduct) router.push("/products");
   return (
     <Layout>
       <h1>New Product</h1>
