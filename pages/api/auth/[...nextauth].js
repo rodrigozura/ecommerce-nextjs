ec9bade5635ecfcebcc775a4a5a1600a4f2f2e8b
rodrigozura <rodrigozura@gmail.com>
2023-05-20 12:48:27 -0300

    verify emails on apis calls

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth";
+import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
 const adminEmails = ["rodrigozura@gmail.com"];
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
@@ -23,4 +23,13 @@ export default NextAuth({
       }
     },
   },
-});
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    throw "not an admin";
+  }
+}
--- pages/api/products.js
+++ pages/api/products.js
@@ -4,6 +4,7 @@ import { Product } from "@/models/Product";
 export default async function handler(req, res) {
   const { method } = req;
   await mongooseConnect();
+  await isAdminRequest(req, res);
 
   if (method === "GET") {
     if (req.query?.id) {
